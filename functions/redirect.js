34583967ca0a8adb773fd505fe0833cf98920f1e
vlangman <vlangman00@gmail.com>
2019-01-09 12:26:35 +0200

    adding redirecting server

---
+++ functions/redirect.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors({ origin: true }));
+
+
+app.all('*', function(req, res) {
+    res.redirect("https://socialspace.co.za");
+});
+
+app.use(function(request, response){
+    if(!request.secure){
+      response.redirect("https://socialspace.co.za");
+    }
+});
+
+app.listen(80 ,function(){console.log("redirect server listening on port 80")});
